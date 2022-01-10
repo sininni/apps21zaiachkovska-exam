55649a7f9dbd2a140a597b7cbca7198e55efe2b0
sininni <73228110+sininni@users.noreply.github.com>
2022-01-10 15:20:12 +0200

    commit

--- src/main/java/app/JSONApp.java
+++ src/main/java/app/JSONApp.java
@@ -3,10 +3,13 @@ package app;
 import domain.*;
 import json.*;
 
+import java.util.List;
+
 /**
  * Created by Andrii_Rodionov on 1/3/2017.
  */
 public class JSONApp {
+    private static JsonObject obj;
     public static void main(String[] args) {
         Json jYear = new JsonNumber(2);
         print(jYear); // 2
@@ -16,15 +19,29 @@ public class JSONApp {
 
         JsonPair name = new JsonPair("name", new JsonString("Andrii"));
         JsonPair surname = new JsonPair("surname", new JsonString("Rodionov"));
-        JsonPair marks = new JsonPair("marks", jMarks);
+//        JsonPair marks = new JsonPair("marks", jMarks);
+
+
+        JsonObject object1 = new JsonObject(new JsonPair("course", new JsonString("OOP")),
+                new JsonPair("mark", new JsonNumber(3)), new JsonPair("passed", new JsonBoolean(true)));
+
+        JsonObject object2 = new JsonObject(new JsonPair("course", new JsonString("English")),
+                new JsonPair("mark", new JsonNumber(5)), new JsonPair("passed", new JsonBoolean(true)));
+
+        JsonObject object3 = new JsonObject(new JsonPair("course", new JsonString("Math")),
+                new JsonPair("mark", new JsonNumber(2)), new JsonPair("passed", new JsonBoolean(false)));
+
+        JsonArray jsonArray = new JsonArray(object1, object2, object3);
+
         JsonPair year = new JsonPair("year", jYear);
-        JsonObject jsonObj = new JsonObject(name, surname, year, marks);
+        JsonObject jsonObj = new JsonObject(name, surname, year, new JsonPair("exams", jsonArray));
+        obj = jsonObj;
         print(jsonObj); // {'name': 'Andrii', 'surname': 'Rodionov', 'year': 2, 'marks': [3, 4]}
 
         print(jsonObj.projection("surname", "age", "year", "marks")); // {'surname': 'Rodionov', 'year': 2, 'marks': [3, 4]}
 
         BasicStudent basicStudent = new BasicStudent("Andrii", "Rodionov", 2);
-        print(basicStudent.toJsonObject()); // {'name': 'Andrii', 'surname': 'Rodionov', 'year': 2}
+//        print(basicStudent.toJsonObject()); // {'name': 'Andrii', 'surname': 'Rodionov', 'year': 2}
 
     }
 
@@ -33,8 +50,10 @@ public class JSONApp {
     }
 
     public static JsonObject sessionResult() {
-        JsonObject jsonObject = null;
-        // ToDo
+        String[] strings = new String[0];
+        main(strings);
+        JsonObject jsonObject = obj;
+
         return jsonObject;
     }
 }
